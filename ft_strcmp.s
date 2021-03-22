777608edf54587947d143e5a0d3db4db223490aa
ysoroko <ysoroko@student.s19.be>
2021-03-22 14:25:27 +0100

    ft_strcmp is now perfeclty functionnal and commented

--- ft_strcmp.s
+++ ft_strcmp.s
@@ -1,5 +1,5 @@
 ;Prototype in C: [int	ft_strcmp(char *a, char *b)]
-; returns -1 / 0 / 1 if a < b / a = b / a > b
+;returns the difference between first two different chars in a & b
 
 ;rax	=	return value registry
 ;rdi	=	first argument registry
@@ -13,8 +13,8 @@ global	_ft_strcmp
 
 _ft_strcmp:
 	xor		rax, rax					;rax = 0
-	xor		r8, r8						;r8 = 0
-	xor		r9, r9						;r9 = 0
+	xor		r8, r8						;r8 = 0 (r8b (aka lower byte of r8) is used to store a char)
+	xor		r9, r9						;r9 = 0 (r9b (aka lower byte of r9) is used to store a char)
 	cmp		rdi, 0						;if (!str_a)
 	je		_return_zero				;return (0);
 	cmp		rsi, 0						;if (!str_b)
@@ -23,14 +23,14 @@ _ft_strcmp:
 	jmp		_compare					;jump to compare function
 
 _compare:
-	mov		r8b, [rdi + r10]			;store the value of str_a[i] in r8d
-	mov		r9b, [rsi + r10]			;store the value of str_b[i] in r9d
+	mov		r8b, [rdi + r10]			;store the value of str_a[i] in r8b
+	mov		r9b, [rsi + r10]			;store the value of str_b[i] in r9b
 	cmp		r8b, 0						;if (!str_a[i])
-	je		_compare_chars				;compare_chars()
+	je		_compare_chars				;jump to compare_chars()
 	cmp		r9b, 0						;if (!str_b[i])
-	je		_compare_chars				;compare_chars()
-	cmp		r8, r9
-	jne		_compare_chars						;if (str_a[i] != str_b[i])
+	je		_compare_chars				;jump to compare_chars()
+	cmp		r8, r9						;if (str_a[i] != str_b[i])
+	jne		_compare_chars				;jump to compare_chars()
 	inc		r10							;i++
 	jmp		_compare					;jump back to the start of _count
 
@@ -38,15 +38,6 @@ _return_zero:
 	ret									;returns rax (which is currently equal to 0)
 
 _compare_chars:
-	sub		r8, r9
-	mov		rax, r8						;if (str_a[i] == str_b[i]) { return (0); }
-	ret									;if (str_a[i] > str_b[i]) { return (1); }
-										;if (str_a[i] < str_b[i]) { return (-1); }
-	
-_return_one:
-	inc		rax							;rax++
-	ret									;return rax
-
-_return_minus_one:
-	dec		rax							;rax--
+	sub		r8, r9						;str_a[i] = str_a[i] - str_b[i];
+	mov		rax, r8						;rax = str_a[i] (which is currently equal to str_a[i] - str_b[i])
 	ret									;return rax
